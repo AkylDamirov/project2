d01476b9338770015408ed51deb78ee9ee293e75
Akyl <akyl@gmail.com>
2024-04-24 17:58:17 +0200

    delete4

--- get_job_2/job_app/admin.py
+++ get_job_2/job_app/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from .models import JobsModel, Cart
-# ,Customer,Order,OrderItem
-# Register your models here.
 admin.site.register(JobsModel)
 admin.site.register(Cart)
-# admin.site.register(Customer)
-# admin.site.register(Order)
-# admin.site.register(OrderItem)
+
